cba1245ccef9adf1da13ade0823032bc79b16666
khanhnhan5391@gmail.com <khanhnhan5391@gmail.com>
2015-08-29 17:13:32 -0700

    Removing unused js files

--- www/index.php
+++ www/index.php
@@ -69,7 +69,6 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         <link rel="icon" type="image/jpeg" href="han.jpg">
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
 
     <body>
@@ -92,20 +91,9 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
-
         <script src="//mandarinspot.com/static/mandarinspot.min.js" charset="UTF-8"></script>
-        <script>mandarinspot.annotate();</script>
+        <script>$(function() { mandarinspot.annotate(); })</script>
     </body>
 </html>
