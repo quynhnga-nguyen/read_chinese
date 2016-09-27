6ee464768db2dab39ce32ada39dbe48da9819c75
Quynh Nga Nguyen <qn281091@gmail.com>
2016-09-26 17:44:23 -0700

    Add google verification

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 script_dir=$(dirname "$0")
-rsync -av --exclude .git $script_dir/ "$1@learn-chinese.cloudapp.net:/var/sites/learn-chinese"
+#rsync -av --exclude .git $script_dir/ "$1@learn-chinese.cloudapp.net:/var/sites/learn-chinese"
+rsync -av --exclude .git $script_dir/ "$1@learn-chinese.cloudapp.net:/home/nga/read_chinese"
 
